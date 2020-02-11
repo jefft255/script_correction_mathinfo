5ba2056d7d411b412376dff1922324cb691a252b
Loïc Ricaud <loic.ricaud.1@ulaval.ca>
2020-02-11 09:48:42 -0500

    Nombre d'arguments.

--- correction.py
+++ correction.py
@@ -99,7 +99,7 @@ def correction_choices(correction, number, default):
 
 
 if __name__ == "__main__":
-    if len(sys.argv[1]) != 4:
+    if len(sys.argv[1]) != 3:
         print("Mauvais nombre d'argument. Utilisation: python correction.py numéro note_maximale dossier_équipes")
 
     number = sys.argv[1]
