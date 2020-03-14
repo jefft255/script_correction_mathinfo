6cc03bb4f6d0ff59db66ea3e4ed3e4ff164e5443
Loïc Ricaud <loic.ricaud.1@ulaval.ca>
2020-03-14 09:53:00 -0400

    Simplification.

--- niveau_de_correction.py
+++ niveau_de_correction.py
@@ -11,16 +11,10 @@ if __name__ == "__main__":
     for team_folder in sorted(teams_path.glob("Equipe *"),
                               key=lambda x: int(str(x.name).split(' ')[-1])):
         for exercise_number in range(1, number_of_exercises + 1):
-            path = Path(team_folder / f"correction{exercise_number}.txt")
-            try:
-                exercise_file = open(path, encoding='utf-8')
-                exercise_file.close()
-            except OSError:
-                print(f"ERREUR: Le fichier {path} est introuvable.")
+            correction_file_path = Path(team_folder / f"correction{exercise_number}.txt")
+            if not correction_file_path.exists():
+                print(f"Le fichier {correction_file_path} est introuvable.")
                 number_of_exercises_not_corrected += 1
-            except ValueError:
-                exercise_file = open(path, encoding='iso-8859-1')
-                exercise_file.close()
     number_of_teams = len([team for team in teams_path.iterdir() if team.is_dir()])
     print(f"La correction est faite à "
           f"{100 * (1 - number_of_exercises_not_corrected / (number_of_exercises * number_of_teams))}%.")
